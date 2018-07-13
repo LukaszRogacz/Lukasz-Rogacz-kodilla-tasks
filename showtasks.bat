c7a5f7a9b270b082d586b72475aa13d1653dd1e8
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-07-13 20:51:33 +0200

    17.4 Stworzenie skryptu shellowego. Skrypt do uruchamiania przeglądarki

---
+++ showtasks.bat
@@ -0,0 +1,17 @@
+call runcrud
+if "%ERRORLEVEL%" == "0" goto runbrowser
+echo.
+echo runcrud has errors
+goto fail
+
+:runbrowser
+explorer "http://localhost:8080/crud/v1/task/getTasks"
+goto end
+
+:fail
+echo.
+echo Errors occured in showtasks file.
+
+:end
+echo.
+echo Work for showtasks script is finished.
