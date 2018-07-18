625f608461882542b8b85704a5edeb0e3c442801
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-07-18 21:25:42 +0200

    18.2 Rest template

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication
+public class TasksApplication{
 		//extends SpringBootServletInitializer
-{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
