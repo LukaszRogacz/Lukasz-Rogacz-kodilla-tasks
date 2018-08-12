5c68006e3bb587e58524421f4294f91c657e97d4
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-08-12 21:48:10 +0200

    19.3 Wysyłanie maili z aplikacji

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,47 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.TasksApplication;
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        //Given
+        Mail mail=new Mail("lukasz_rogacz@wp.pl","Temat jak ta lala","Niosę wiadomość niezwykłą...",null);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        if(mail.getToCC()!=null && !mail.getToCC().equals("")) {
+            mailMessage.setCc(mail.getToCC());
+        }
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        verify(javaMailSender,times(1)).send(mailMessage);
+    }
+}
